37afc4699de47f0e4a6f300933de3b3e28f3fc91
alartuka <tukaalarbi@gmail.com>
2024-02-19 18:38:40 -0500

    first_update

---
+++ main/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NewsFeed from './components/NewsFeed';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ZAMAN NEWS</h1>
+
+      </header>
+      <NewsFeed />
+    </div>
+  );
+}
+
+export default App;
